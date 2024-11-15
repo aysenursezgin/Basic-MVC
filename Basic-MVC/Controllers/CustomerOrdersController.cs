a1429a5488492e110c658a9b4a19668ce71ae3a1
aysenursezgin <115412288+aysenursezgin@users.noreply.github.com>
2024-11-16 02:01:48 +0300

    Proje dosyası ekle.

---
+++ Basic-MVC/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,35 @@
+﻿using Basic_MVC.Models;
+using Basic_MVC.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+public class CustomerOrdersController : Controller
+{
+    public ActionResult Index()
+    {
+        // Örnek müşteri oluşturma
+        var customer = new Customer
+        {
+            Id = 1,
+            FirstName = "Ahmet",
+            LastName = "Yılmaz",
+            Email = "ahmet.yilmaz@example.com"
+        };
+
+        // Örnek siparişler oluşturma
+        var orders = new List<Order>
+        {
+            new Order { Id = 1, ProductName = "Laptop", Price = 1500.00m, Quantity = 1 },
+            new Order { Id = 2, ProductName = "Telefon", Price = 500.00m, Quantity = 2 }
+        };
+
+        // ViewModel oluşturma
+        var viewModel = new CustomerOrderViewModel
+        {
+            Customer = customer,
+            Orders = orders
+        };
+
+        // ViewModel'i view'a iletme
+        return View(viewModel);
+    }
+}
---
+++ Basic-MVC/ViewModel/CustomerOrderViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Basic_MVC.ViewModel
+{
+    using Basic_MVC.Models;
+    using System.Collections.Generic;
+
+    public class CustomerOrderViewModel
+    {
+        public Customer Customer { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+
+}
